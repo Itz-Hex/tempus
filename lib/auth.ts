62cb7cd0ee79f7427f831b58e5fb6559313a7aa6
Itz-Hex <ollie@olliewells.co.uk>
2026-09-05 13:17:23 +0100

    started password reset

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,9 +12,12 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }
+    },
   },
   emailAndPassword: {
     enabled: true,
+    sendResetPassword: async ({ user, url, token }, request) => {
+      // continue
+    },
   },
 });
